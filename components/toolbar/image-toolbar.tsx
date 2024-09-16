e657bcd64436fbccb5794a7cd8c160768787dacb
André Suárez Schou <andre_suarez_schou@hotmail.com>
2024-09-16 14:20:02 +0200

    background functionality
    removed the bg removal and implemented the bg replace

--- components/toolbar/image-toolbar.tsx
+++ components/toolbar/image-toolbar.tsx
@@ -1,11 +1,12 @@
-import BgRemove from "./bg-remove"
+import AIBackgroundReplace from "./bg-replace"
 import GenRemove from "./gen-remove"
 
 export default function ImageTools() {
   return (
     <>
       <GenRemove />
-      <BgRemove />
+      <AIBackgroundReplace />
+      
     </>
   )
 }
